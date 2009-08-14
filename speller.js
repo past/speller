60cd20f30fe8cec9ab6031a80b275927accaffde
Panagiotis Astithas <pastith@gmail.com>
2009-08-14 23:48:15 +0300

    Fix alteration and insertion, by properly iterating over the alphabet characters.

--- speller.js
+++ speller.js
@@ -64,25 +64,28 @@ speller.max = function(candidates) {
 	return Math.max.apply(null, arr);
 };
 
-speller.nWords = {};
+exports.nWords = speller.nWords = {};
+
+speller.alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
 speller.edits = function (word) {
 	var results = [];
-	var i, c;
 	// deletion
-	for (i=0; i < word.length; i++)
+	for (var i=0; i < word.length; i++)
 	    results.push(word.slice(0, i) + word.slice(i+1));
 	// transposition
 	for (i=0; i < word.length-1; i++)
 	    results.push(word.slice(0, i) + word.slice(i+1, i+2) + word.slice(i, i+1) + word.slice(i+2));
 	// alteration
 	for (i=0; i < word.length; i++)
-	    for(c='a'; c <= 'z'; c++)
+	    speller.alphabet.forEach(function(c) {
 	        results.push(word.slice(0, i) + c + word.slice(i+1));
+		});
 	// insertion
 	for (i=0; i <= word.length; i++)
-	    for(c='a'; c <= 'z'; c++)
+	    speller.alphabet.forEach(function(c) {
 	        results.push(word.slice(0, i) + c + word.slice(i));
+		});
 	return results;
 };
 
